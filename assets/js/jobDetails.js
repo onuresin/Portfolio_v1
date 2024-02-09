127d93e751f37856900ef1e4f6508a29b6d8b6da
Onur Esin <onuresn034@gmail.com>
2024-02-09 23:43:53 +0300

    mobil tasarım tamamlandı eksikler giderildi, içerik web tam sürümde değişecek

--- assets/js/jobDetails.js
+++ assets/js/jobDetails.js
@@ -1,9 +1,21 @@
-function showDetails(detailId) {
+function showDetails(titleId, detailId) {
+    let allTitles = document.getElementsByClassName("title");
     let allDetails = document.getElementsByClassName("expDetails");
+
+    for (let i=0; i < allTitles.length; i++) {
+        allTitles[i].style.backgroundColor = "";
+        allTitles[i].style.color = "";
+        allTitles[i].classList.remove('selected');
+    }
 
     for (let i=0; i < allDetails.length; i++) {
         allDetails[i].style.display = 'none';
     }
+
+    let selectedTitle = document.getElementById(titleId);
+    selectedTitle.style.backgroundColor = "black";
+    selectedTitle.style.color = "white";
+    selectedTitle.classList.add('selected');
 
     let selectedDetail = document.getElementById(detailId);
     selectedDetail.style.display = 'block';
