fd1b5865e3efee7cf4d219dd5f2bffaac7b89225
Onur Esin <onuresn034@gmail.com>
2024-02-10 19:04:45 +0300

    localstorage eklendi

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -5,11 +5,21 @@ const body = document.body
 darkModeBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
         body.classList.add("dark-mode");
+        localStorage.setItem('dark-mode', 'true');
     });
 });
 
 lightModeBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
         body.classList.remove("dark-mode");
+        localStorage.removeItem('dark-mode');
     });
 });
+
+window.addEventListener('load', () => {
+    const darkMode = localStorage.getItem('dark-mode');
+
+    if (darkMode === 'true') {
+        body.classList.add("dark-mode");
+    }
+})
