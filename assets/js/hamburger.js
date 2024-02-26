f8f5920d9749413099843ce5d4b9d99808390988
Onur Esin <onuresn034@gmail.com>
2024-02-26 03:01:40 +0300

    modal içinde ki dil seçim seçeneklerinin çalışmama sorunu giderildi

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -1,10 +1,21 @@
 function toggleMenu() {
     const hamburger = document.querySelector('.hamburger');
     const modal = document.querySelector('.modal');
+    const select = document.querySelector('#langModal');
 
     hamburger.classList.toggle('active');
     modal.classList.toggle('active');
 
+    if (modal.classList.contains('active')) {
+        select.addEventListener('click', stopPropagation);
+    } else {
+        select.removeEventListener('click', stopPropagation);
+    }
+
     modal.removeEventListener('click', toggleMenu);
     modal.addEventListener('click', toggleMenu);
+
+    function stopPropagation(event) {
+        event.stopPropagation();
+    } // bu fonksiyon yukarıda tanımladığım modal içinde ki select elementinin tıklanmasına olanak sağlıyor
 }
